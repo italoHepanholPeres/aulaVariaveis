2133c59dd3a1b95c637e3248472b9172fd8c9e5f
italoHespanholPeres <pereshespanhol1@gmail.com>
2022-08-22 22:27:40 -0300

    indetação do código

--- aulaVariaveis/src/aulaIf/AulaIf.java
+++ aulaVariaveis/src/aulaIf/AulaIf.java
@@ -4,30 +4,22 @@ public class AulaIf {
 
 	public static void main(String[] args) {
 
-		int valorInteiro = 10;
-		
-		if(valorInteiro > 11) {
+		int valorInteiro = 9;
+
+		if (valorInteiro > 11) {
 			System.out.println("verdade");
-		}	
-	    else if (3 > 4)	{System.out.println("verdade 2");
-		}
-	     if (5 < 4 && 5 > 8) { 
-		System.out.println("verdade 3");
-	     }else {
+		} else if (7 > 2) {
+			System.out.println("verdade 2");
+		} else if (3 < 4 && 5 > 8) {
+			System.out.println("verdade 3");
+		} else {
 			System.out.println("todas sao falsas");
 		}
-		
-	     
-	
-	    }
-}		
-		
-		
-		
-		
-	
-			
-		
-	
 
+	}
+}
 
+//   ctrl a seleciona tudo
+//   ctrl shift f indenta
+//   ctrl z cancela as ações
+//   ctrl y desfaz as ações
